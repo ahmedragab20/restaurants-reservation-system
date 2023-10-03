95e4c200d60a6ddaec3ecd0d4b558ead6bdbe581
ahmedragab <ahmedragab20901@gmail.com>
2023-10-03 05:21:20 +0300

    hellow

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <template>
-  <div id="app" class="p-3 border">Hello wrold</div>
+  <div id="app" class="p-3 border flex">
+    <!-- components:
+          - dropdown (select/multiselect)
+          - dialog
+          - input
+          - button
+          - tags
+          - time range picker
+          - hint
+    -->
+
+    <div class="border rounded-xl overflow-hidden">
+      <select
+        name="na"
+        id="od"
+        class="px-2 py-1 appearance-none focus:outline-none"
+      >
+        <option value="0">00</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+      -
+      <select
+        name="na"
+        id="od"
+        class="px-2 py-1 appearance-none focus:outline-none"
+      >
+        <option value="0">00</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
   name: "App",
-  async created() {
-    const res = await axios.get("/branches");
-    console.log(res);
-  },
 };
 </script>
